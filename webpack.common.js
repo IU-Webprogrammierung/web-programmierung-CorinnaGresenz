29dad39799150f990d3da258967c75d312969ddf
CorinnaGresenz <corinna.gresenz@iubh-fernstudium.de>
2024-07-20 17:15:38 +0200

    Add file loader to webpack for local font

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: {
-    app: './js/app.js',
+    app: "./js/app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    filename: './js/app.js',
+    filename: "./js/app.js",
   },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,22 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'style', to: 'style' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: '404.html', to: '404.html' },
+        { from: "img", to: "img" },
+        { from: "style", to: "style" },
+        { from: "js/vendor", to: "js/vendor" },
+        { from: "favicon.ico", to: "favicon.ico" },
+        { from: "404.html", to: "404.html" },
+        { from: "assets", to: "assets" },
       ],
     }),
   ],
