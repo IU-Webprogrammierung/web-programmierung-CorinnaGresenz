50dbaf85f9b40f5203ee7834c3867e4cfafbd56a
CorinnaGresenz <corinna.gresenz@iubh-fernstudium.de>
2024-07-24 22:08:57 +0200

    Add a image gallery (modal based)

--- js/app.js
+++ js/app.js
@@ -10,4 +10,38 @@ $(document).ready(function () {
   function submitContactForm() {
     alert("Das Kontaktformular wurde abgeschickt!");
   }
+
+  let modal = $("#myModal");
+  let slides = $(".slide");
+  let images = $(".image-gallery .image");
+  let slideIndex = 0;
+
+  images.each(function (index) {
+    $(this).on("click", function () {
+      modal.css("display", "flex");
+      showSlide(index);
+    });
+  });
+
+  $(".close").on("click", function () {
+    modal.css("display", "none");
+  });
+
+  function showSlide(index) {
+    slides.each(function () {
+      $(this).removeClass("active");
+    });
+    slideIndex = index;
+    $(slides[slideIndex]).addClass("active");
+  }
+
+  $(".next").on("click", function () {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+  });
+
+  $(".prev").on("click", function () {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+  });
 });
