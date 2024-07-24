d3840ecf7328d2238a2b1468678076bc06c96846
CorinnaGresenz <corinna.gresenz@iubh-fernstudium.de>
2024-07-24 22:08:41 +0200

    Refactor old submit logic into jQuery submit (form) logic

--- js/app.js
+++ js/app.js
@@ -2,4 +2,12 @@ import $ from "jquery";
 
 $(document).ready(function () {
   console.log("jQuery is ready!");
+
+  $("#submitButton").on("click", function () {
+    submitContactForm();
+  });
+
+  function submitContactForm() {
+    alert("Das Kontaktformular wurde abgeschickt!");
+  }
 });
